56c6fd34ac77bb4554e3ef07e692ff4f8ea835a8
Chani17 <cucute8878@naver.com>
2025-08-05 21:13:30 +0900

    Basic Component

---
+++ BASIC/src/components/Main.jsx
@@ -0,0 +1,11 @@
+const Main = () => {
+    const number = 10;
+    return (
+        <main>
+            <h1>main</h1>
+            <h2>{number % 2 === 0 ? "짝수" : "홀수"}</h2>
+        </main>
+    );
+};
+
+export default Main;
