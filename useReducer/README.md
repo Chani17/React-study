360f8fb45c2c644697bdf6a664cc81852341eeda
Chani17 <cucute8878@naver.com>
2025-08-12 22:24:37 +0900

    Refactor todo-list-app to use useReducer instead of useState

--- useReducer/README.md
+++ useReducer/README.md
@@ -1,8 +1,10 @@
 # useReducer
-component 내부에 새로운 State를 생성하는 React Hook
+component 내부에 새로운 State를 생성하는 React Hook  
 모든 useState는 useReducer로 대체 가능
-> useState와의 차이점
+> **useState와의 차이점**  
 > 상태 관리 코드를 component 외부로 분리할 수 있음!
+
+<br>
 
 아래의 코드처럼 useState는 useState를 선언한 component 내부에서 상태 관리 코드(onCreate)를 작성해야하는 것이다.
 ```
@@ -24,8 +26,9 @@ function App() {
 ```
 
 <br>
-하지만, useReducer를 사용하면?
-component 외부에 상태 관리 코드를 분리할 수 있다.
+하지만, useReducer를 사용하면?<br>
+component 외부에 상태 관리 코드를 분리할 수 있다
+
 ```
 function reducer() {
   // ...
--- useReducer/src/App.jsx
+++ useReducer/src/App.jsx
@@ -1,6 +1,9 @@
-import { useRef, useState } from "react";
+import { useReducer, useRef } from "react";
 import './App.css';
-import Exam from './components/Exam';
+import Editor from './components/Editor';
+import Header from './components/Header';
+import List from './components/List';
+// import Exam from './components/Exam';
 
 const mockData = [
   {
@@ -23,49 +26,61 @@ const mockData = [
   },
 ];
 
+function reducer(state, action) {
+  switch(action.type) {
+    case 'CREATE':
+      return [action.data, ...state];
+    case 'UPDATE':
+      return state.map((item) =>
+        item.id === action.targetId
+          ? {...item, isDone: !item.isDone}
+          : item);
+    case 'DELETE':
+      return state.filter((item) => item.id !== action.targetId);
+    default:
+      return state;
+  }
+}
 
 function App() {
-  const [todos, setTodos] = useState(mockData);
+  const [todos, dispatch] = useReducer(reducer, mockData);
   const idRef = useRef(3);
 
   const onCreate = (content) => {
-    const newTodo = {
-      id: idRef.current++,
-      isDone: false,
-      content: content,
-      date: new Date().getTime(),
-    };
-
-    setTodos([newTodo, ...todos]);
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: idRef.current++,
+        isDone: false,
+        content: content,
+        date: new Date().getTime(),
+      },
+    });
   };
 
   const onUpdate = (targetId) => {
-    // todos State의 값들 중에
-    // targetId와 일치하는 Id를 갖는 todo item의 isDone 변경
-    
-    // 인수: todos 배열에서 targetId와 일치하는 id를 갖는 요소의 데이터만 딱 바꾼 새로운 배열
-    setTodos (
-      todos.map((todo) =>
-        todo.id === targetId
-          ? {...todo, isDone: !todo.isDone }
-          : todo)
-    );
+    dispatch({
+      type: "UPDATE",
+      targetId: targetId,
+    });
   };
 
   const onDelete = (targetId) => {
-    // 인수: todos 배열에서 targetId와 일치하는 Id를 갖는 요소만 삭제한 새로운 배열
-    setTodos(todos.filter((todo) => todo.id !== targetId));
+    dispatch({
+      type: 'DELETE',
+      targetId: targetId,
+    });
   };
 
   return (
     <div className="App">
-      <Exam />
-      {/* <Header />
+      {/* <Exam /> */}
+      <Header />
       <Editor onCreate={onCreate} />
       <List todos={todos}
         onUpdate={onUpdate}
         onDelete={onDelete}
-      /> */}
+      />
     </div>
   )
 }
